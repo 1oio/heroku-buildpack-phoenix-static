8edfcf75394e1d06fd922a492a62ed68af11e316
Andrej Jančič <andrejjancic@gmail.com>
2021-04-16 12:18:50 +0200

    create node_modules dir when not there

--- lib/build.sh
+++ lib/build.sh
@@ -128,6 +128,7 @@ install_and_cache_node_modules() {
   if [ -d $app_cache_dir/node_modules ]; then
     info "Loading node modules from cache"
     info "cp -R $app_cache_dir/node_modules/* node_modules/"
+    mkdir -p node_modules
     cp -R $app_cache_dir/node_modules/* node_modules/
   fi
 
